9a973e980f51a1c64231cc45feb6504eef104897
vbastianpc <vbastianpalavecino@gmail.com>
2021-09-18 19:24:14 -0300

    add parse_vtt

--- api_subtitles.py
+++ api_subtitles.py
@@ -1,6 +1,9 @@
 import requests
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
+from io import StringIO
+
+import webvtt
 
 
 API_MEDIATOR = 'https://b.jw-cdn.org/apis/mediator/v1/media-items/{code_lang}/{lank}?clientType=www'
@@ -27,19 +30,6 @@ def get_code_lang(locale):
         raise CodeLangNotFound(locale)
 
 
-def get_subtitles(url):
-    up = urlparse(url)
-    pq = parse_qs(up.query)
-    if up.path == '/finder':
-        code_lang, lank = pq['wtlocale'][0], pq['lank'][0]
-    elif '/mediaitems/' in up.fragment:
-        locale, _, category, lank = up.fragment.split('/')
-        code_lang = get_code_lang(locale)
-    else:
-        raise ValueError(f'{url!r} it is not a valid url')
-    return get_subtitles_from_mediator(code_lang, lank)
-
-
 def get_subtitles_from_mediator(code_lang, lank):
     url = API_MEDIATOR.format(code_lang=code_lang, lank=lank)
     data = requests.get(url).json()
@@ -52,6 +42,29 @@ def get_subtitles_from_mediator(code_lang, lank):
     return file['subtitles']['url']
 
 
+def get_url_subtitles(url):
+    up = urlparse(url)
+    pq = parse_qs(up.query)
+    if up.path == '/finder':
+        code_lang, lank = pq['wtlocale'][0], pq['lank'][0]
+    elif '/mediaitems/' in up.fragment:
+        locale, _, category, lank = up.fragment.split('/')
+        code_lang = get_code_lang(locale)
+    else:
+        raise ValueError(f'{url!r} it is not a valid url')
+    return get_subtitles_from_mediator(code_lang, lank)
+
+
+def parse_vtt(text_vtt):
+    text = ''
+    for caption in webvtt.read_buffer(StringIO(text_vtt)):
+        print(f'{caption.text!r}')
+        text += caption.text.replace('\n', ' ') + '\n'
+        if caption.text.strip().endswith('.'):
+            text += '\n'
+    return text
+
+
 if __name__ == '__main__':
     urls = [
         'https://www.jw.org/finder?srcid=share&wtlocale=S&lank=docid-502200113_1_VIDEO',
@@ -60,5 +73,5 @@ if __name__ == '__main__':
         'https://www.jw.org/finder?srcid=share&wtlocale=S&lank=pub-jwb-082_1_VIDEO'
     ]
     for url in urls:
-        print(get_subtitles(url))
+        print(get_url_subtitles(url))
     print('FIN')
