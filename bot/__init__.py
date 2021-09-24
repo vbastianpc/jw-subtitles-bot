495d2e5b1dfa2ba6cdde9e551b2a0be868528add
vbastianpc <vbastianpalavecino@gmail.com>
2021-09-24 15:37:29 -0300

    timezone UTC server

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,8 +11,7 @@ class Formatter(logging.Formatter):
     """override logging.Formatter to use an aware datetime object"""
     def converter(self, timestamp):
         dt = datetime.datetime.fromtimestamp(timestamp)
-        tzinfo = pytz.timezone('America/Santiago')
-        # tzinfo = pytz.timezone('UTC')
+        tzinfo = pytz.timezone('UTC')
         return tzinfo.localize(dt)
 
     def formatTime(self, record, datefmt=None):
